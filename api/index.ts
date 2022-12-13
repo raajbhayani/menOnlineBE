02c2dc59940d3eab4fd770a39332392408d62c4f
Raj Bhayani <99900996+Rajbhayani4@users.noreply.github.com>
2022-12-13 19:39:34 +0530

    remove send email

--- api/index.ts
+++ api/index.ts
@@ -30,19 +30,19 @@ app.use('/subCategory', Routes?.subCategoryRouter);
 app.use('/workHistory', Routes?.workHistoryRouter);
 
 app.get('/', async (req: Request, res: Response) => {
-    const name: string = 'rajbhayani.scaleteam@gmail.com';
-    const obj: any = {
-        name,
-        otp: "7895",
-        propose: "Account verification code"
-    }
-    let data: any;
-    sendEmail(name, 'Login', obj).then((res: any) => data = res).catch((error: any) => data = error.message)
-    console.log("🚀 ~ file: index.ts:47 ~ app.get ~ data", data)
+    // const name: string = 'rajbhayani.scaleteam@gmail.com';
+    // const obj: any = {
+    //     name,
+    //     otp: "7895",
+    //     propose: "Account verification code"
+    // }
+    // let data: any;
+    // sendEmail(name, 'Login', obj).then((res: any) => data = res).catch((error: any) => data = error.message)
+    // console.log("🚀 ~ file: index.ts:47 ~ app.get ~ data", data)
     // sendMessage([9925185934], 1234);
     res.json({
         status: "Ok",
-        data: data
+        // data: data
     })
 });
 
